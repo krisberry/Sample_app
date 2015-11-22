78a168945b2acae4b6a395425a55aaab2860134c
“krisberry“ <tina.doskich@gmail.com>
2015-11-22 14:37:38 +0200

    Finish user edit, update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -44,6 +44,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login without remembering" do
     log_in_as(@user, remember_me: '0')
-    assert_not_nil cookies['remember_token']
+    assert_nil cookies['remember_token']
   end  
 end
